aa8f6f10a833203386925ab4900f2e30f8c46962
yujc <yujc.astro@gmail.com>
2013-09-02 17:31:44 -0400

    frac.c - makemass/IMF 2013-09-02 from iMac

--- frac.c
+++ frac.c
@@ -26,7 +26,7 @@ double makemass(double ml, double mh, double x1, double x2, int s)
 {
 	double m,m1=0.08,m2=0.5;
 	double a1=0.3,a2=1.3,a3=2.3;
-	double x,x1,x2;
+	double x;
 	switch (s) {
 		case '3':
 			x = randomz();
@@ -112,19 +112,23 @@ double kroupa_IMF(double ml, double mh)
 
 double IMF(int s, double *a, double *m0)
 {
-	/* s sections, ml = m0[0], mh = m0[s] */
+	/* s sections, ml = m0[0], mh = m0[s], m0[i] < m0[i+1] */
+	/* f(m)dm = ki * m^(-ai) , m0[i] < m < m0[i+1], i=0,1,...,s-1 */
+	double m;
 	double norm;
 	double norm0[s];
-	double a1[s];
-	/*
-	double *norm0;
-	norm0 = (double*) malloc(s*sizeof(double));
-	double *a1;
-	a1 = (double*) malloc(s*sizeof(double));
-	*/
+	double a1;
+	double x0[s];
+	double x;
+	double ki;
+	int i;
+	a1 = 1 - a[0];
+	ki = 1;
+	norm0[0] = (pow(m0[1],a1) - pow(m0[0],a1)) / a1; /* norm0[0] *= ki */
 	for (i=0;i<s;++i) {
 		a1 = 1 - a[i];
-		norm0[i] = (pow(m0[i+1],a1) - pow(m0[i],a1)) / a1;
+		ki *= pow(m0[i],a[i]-a[i-1]);
+		norm0[i] = ki * (pow(m0[i+1],a1) - pow(m0[i],a1)) / a1;
 	}
 	
 	norm = 0;
@@ -172,53 +176,24 @@ void fractal(int StarNum, double D, double mlow, double mhigh, struct vector_s *
 	}
 	double dm = mhigh - mlow;
 
-	double norm1,norm2,norm3,norm;
-	double x1,x2;
-	double ml,mh;
-	int s;
-	if (ml < m1) {
-		s = 3;
-		norm1 = (pow(m1/ml,-a1+1) - 1) / (-a1 + 1);
-		norm2 = (pow(m2/m1,-a2+1) - 1) / (-a2 + 1);
-		norm3 = (pow(mh/m2,-a3+1) - 1) / (-a3 + 1);
-		norm  = 1. / (norm1 + norm2 + norm3);
-	} else if (ml < m2) {
-		s = 2;
-		norm1 = 0;
-		norm2 = (pow(m1/ml,-a2+1) - 1) / (-a2 + 1);
-		norm3 = (pow(mh/m2,-a3+1) - 1) / (-a3 + 1);
-		norm  = 1. / (norm1 + norm2 + norm3);
-	} else {
-		s = 1;
-		norm1 = 0;
-		norm2 = 0;
-		norm3 = (pow(mh/ml,-a3+1) - 1) / (-a3 + 1);
-		norm  = 1. / (norm1 + norm2 + norm3);
-	}
-	x1 = norm * norm1;
-	x2 = norm * (norm1 + norm2);
-	switch (s) {
-		case '3':
-			x = randomz();
-			if (x < x1) {
-				m = general_power_law(ml,m1,a1);
-			} else if (x < x2) {
-				m = general_power_law(m1,m2,a2);
-			} else {
-				m = general_power_law(m2,mh,a3);
-			}
-			break;
-		case '2':
-			if (randomz() < x1) {
-				m = general_power_law(ml,m2,a2);
-			} else {
-				m = general_power_law(m2,mh,a3);
-			}
-			break;
-		case '1':
-			general_power_law(ml,mh,a3);
-			break;
-	}
+	int s=2;
+	double alpha[s];
+	double mb[s+1];
+	double xb;
+	double norm;
+	double norm0[s];
+	alpha[0] = 1.3;
+	alpha[1] = 2.3;
+	mb[0] = mlow;
+	mb[1] = 0.5;
+	mb[2] = mhigh;
+	a1 = 1 - alpha[0];
+	norm0[0] = (pow(mb[0]/mb[1], 1-alpha[1]) - 1) / (1-alpha[1]);
+	a1 = 1 - alpha[1];
+	norm0[1] = (pow(mb[2]/mb[1],a1) - 1) / a1;
+	norm = 1./(norm0[0] + norm0[1]);
+	xb = norm0[0]*norm;
+
 	int numc=1;
 	struct node *child,*parent,*headc,*headp;
 	if (NULL == (headc = (struct node*) malloc(LEN))) {
@@ -305,6 +280,7 @@ void fractal(int StarNum, double D, double mlow, double mhigh, struct vector_s *
 				if (randomz() < mp){
 
 					// generate the mass of sub-nodes:
+					/*
 					do {
 						m = mlow + dm * randomz();
 						if (m<m1){
@@ -315,6 +291,12 @@ void fractal(int StarNum, double D, double mlow, double mhigh, struct vector_s *
 							temp = pow(m,-a2);
 						}
 					} while (upper*randomz() > temp);
+					*/
+					if (randomz() < xb) {
+						general_power_law(mb[0],mb[1],alpha[0]);
+					} else {
+						general_power_law(mb[1],mb[2],alpha[1]);
+					}
 //					m = mass[i];
 					// m = makemass( mlow,mhigh );
 						//	may be used some day...
